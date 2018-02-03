7704454e0845aae5cf386339d285e1febf7078a7
Tim <tim@libens.net>
2018-02-03 01:09:37 -0800

    reordered a bit

--- bootstrap.el
+++ bootstrap.el
@@ -1,18 +1,16 @@
 ;; (with-temp-buffer (url-insert-file-contents "https://raw.githubusercontent.com/brool/emacs-remote-bootstrap/master/bootstrap.el") (eval-buffer))
 
+;; You may not need all these archives
+(setq package-archives '(("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")))
+
 (defun load-file-remotely (url)
   (with-temp-buffer (url-insert-file-contents url) (eval-buffer)))
 
 ;; Bootstrap up the Try package
-
 (defvar try-tmp-dir (make-temp-file "try" t))
-
-;; You may not need all these
-
-(setq package-archives '(("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
-                         ("gnu" . "http://elpa.gnu.org/packages/")))
-
 (setq package-user-dir try-tmp-dir)
+(setq package-alist nil)
 (package-initialize)
 (package-refresh-contents)
 (if (version< emacs-version "25.1")
@@ -20,7 +18,6 @@
   (package-install 'try 'dont-select))
 
 ;; now install everything you need
-
 (try 'use-package)
 (try 'web-mode)
 (try 'ivy)
